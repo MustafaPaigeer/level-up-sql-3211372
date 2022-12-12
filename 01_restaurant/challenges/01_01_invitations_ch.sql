40706fc51932df6bd3cc05a3fe65f1475d52585c
Mustafa Paigeer <43423576+MustafaPaigeer@users.noreply.github.com>
2022-12-12 07:17:05 +0000

    solve first challenge

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,5 @@
 -- Generate a list of customer information.
-select * from customers;
+SELECT FIRSTNAME, LASTNAME, EMAIL FROM Customers
+ORDER BY LASTNAME ASC;
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
